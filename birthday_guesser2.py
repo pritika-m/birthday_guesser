a291f1e52ead6bce06dae2b8274d77f91e4a57c2
pritika-m <152086899+pritika-m@users.noreply.github.com>
2024-01-07 11:41:50 -0500

    Add files via upload

---
+++ birthday_guesser2.py
@@ -0,0 +1,29 @@
+print("Think about a month and day, it can even be your birthday if you wish")
+print("Multiply the month by 2, and then add 5.")
+print("Then multiply that by 50 and then add your day of choice")
+u=int(input("What number did you recieve: "))
+print("Ok, I will guess your birth month. Is your birthday month...")
+if 300<u<399:
+    print("Your birth month is January (1)")
+elif 400<u<499:
+    print("Your birth month is February (2)")
+elif 500<u<599:
+    print("Your birth month is March (3)")
+elif 600<u<699:
+    print("Your birth month is April (4)")
+elif 700<u<799:
+    print("Your birth month is May (5)")
+elif 800<u<899:
+    print("Your birth month is June (6)")
+elif 900<u<999:
+    print("Your birth month is July (7)")
+elif 1000<u<1099:
+    print("Your birth month is August (8)")
+elif 1100<u<1199:
+    print("Your birth month is September (9)")
+elif 1200<u<1299:
+    print("Your birth month is October (10)")
+elif 1300<u<1399:
+    print("Your birth month is November (11)")
+elif 1400<u<1499:
+    print("Your birth month is December (12)")
